fb11ac89e175aa3a2bf0e5e69a74ab5f5ef783b4
David Latorre <david@latorredev.com>
2025-01-18 16:38:08 -0500

    [ADD] add alpha

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,66 @@
-/*
-	Miniport by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+(function ($) {
+    var $window = $(window),
+        $body = $('body'),
+        $nav = $('#nav');
 
-(function($) {
+    // Breakpoints.
+    breakpoints({
+        xlarge: ['1281px', '1680px'],
+        large: ['981px', '1280px'],
+        medium: ['737px', '980px'],
+        small: [null, '736px'],
+    });
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+    // Play initial animations on page load.
+    $window.on('load', function () {
+        window.setTimeout(function () {
+            $body.removeClass('is-preload');
+        }, 100);
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+        // Terminal typing effect.
+        terminalTypingEffect(
+            "#terminal-text",
+            "Hi. I'm David Latorre.\nDevOps/Cloud Engineer/Site Reliability Engineer (SRE)",
+            100
+        );
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Scrolly.
+    $('#nav a, .scrolly').scrolly({
+        speed: 1000,
+        offset: function () {
+            return $nav.height();
+        },
+    });
 
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
+    // Terminal typing effect function.
+    function terminalTypingEffect(target, text, speed) {
+        const element = document.querySelector(target);
+        const cursor = document.createElement("span");
+        cursor.textContent = "_";
+        cursor.classList.add("blinking-cursor");
+        element.innerHTML = ""; // Clear content initially.
+        element.appendChild(cursor);
 
+        let index = 0;
+
+        function type() {
+            if (index < text.length) {
+                const char = text.charAt(index);
+                if (char === "\n") {
+                    element.innerHTML =
+                        element.innerHTML.slice(0, -1) + "<br>" + "_";
+                } else {
+                    element.innerHTML =
+                        element.innerHTML.slice(0, -1) + char + "_";
+                }
+                index++;
+                setTimeout(type, speed);
+            } else {
+                cursor.remove(); // Remove the cursor once typing is complete.
+            }
+        }
+
+        type();
+    }
 })(jQuery);
